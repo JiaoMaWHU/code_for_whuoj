796f9f3b707995d1be16b14c188fff2aa91b89e7
JiaomaWHU <jiaomajiaoma@hotmail.com>
2018-12-01 18:37:11 +0800

    Day12.1

---
+++ ac-whuoj/ac-1/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  ac-whuoj
+//
+//  Created by 角马 on 2018/12/1.
+//  Copyright © 2018 角马. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d",a+b);
+    return 0;
+}
---
+++ ac-whuoj/ac-2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ac-2
+//
+//  Created by 角马 on 2018/12/1.
+//  Copyright © 2018 角马. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
