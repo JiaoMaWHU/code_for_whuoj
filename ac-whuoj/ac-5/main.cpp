6a345165db5054a85320f37b9b7f99ce64d3989b
JiaomaWHU <jiaomajiaoma@hotmail.com>
2018-12-18 20:37:42 +0800

    add O(c) solution for ac-5

--- ac-whuoj/ac-5/main.cpp
+++ ac-whuoj/ac-5/main.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 using namespace std;
-
+/* O(num*value)
 int main(int argc, const char * argv[]) {
     int dp[101][100001];
     int s[101],p[101];
@@ -23,4 +23,22 @@ int main(int argc, const char * argv[]) {
 		cout<<dp[num][value]<<endl;
     }
     return 0;
+}
+*/
+
+// O(value)
+int main(){
+	int dp[100001];
+	int s[101],p[101];
+	int num,value;
+	while (cin>>num){
+		for (int i=0;i<num;i++) cin>>s[i]>>p[i];
+		cin>>value;
+		for (int i=0;i<=value;i++) dp[i]=0;
+		for (int i=0;i<num;i++)
+			for (int j=value;j>=s[i];j--) 
+				if(dp[j]<(dp[j-s[i]]+p[i])) dp[j]=dp[j-s[i]]+p[i];
+		cout<<dp[value]<<endl;
+	}
+	return 0;
 }
