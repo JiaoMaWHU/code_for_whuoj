6ec965d4a47fb801f794c1f0d392fb15648a9c40
JiaomaWHU <jiaomajiaoma@hotmail.com>
2019-01-01 23:52:53 +0800

    first day of 2019

--- ac-whuoj/ac-9/main.cpp
+++ ac-whuoj/ac-9/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+#define MAX 20000;
+typedef struct{
+    int d;//到达某点的最短路径距离
+    int pre;//该最短路径是经过哪个点传过来的，源点或其他某个点
+}path;
 vector<pair<int,int>> graph[100];
 char Elixir[4000];
 int guard[4000][3];
-int dis[100];
+path dis[100];
 
 int main() {
     int n,m,plovers,slovers;
@@ -21,12 +26,6 @@ int main() {
             guard[i][0]=pguard;guard[i][1]=sguard;guard[i][2]=cost;
         }
         cin>>plovers>>slovers;
-        for (int i=0; i<100; i++) {
-            if (!graph[i].empty()) {
-                pair<int,int> state(0,0);
-                graph[i].push_back(state);
-            }
-        }
     }
     return 0;
 }
