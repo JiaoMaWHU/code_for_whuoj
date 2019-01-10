d0fa8106a2e752be611fba6ccb7b5c547adacdd9
JiaomaWHU <jiaomajiaoma@hotmail.com>
2019-01-10 11:21:58 +0800

    ac-16

---
+++ ac-whuoj/ac-16/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+// ~N^lgN,43ms
+int main() {
+    ios_base::sync_with_stdio(false);
+    int n;
+    while (cin>>n) {
+        set<pair<int, int>> p;
+        int max_y,min_y;
+        int x,y;
+        cin>>x>>y;
+        p.insert(make_pair(x, y));
+        max_y = min_y =y;
+        for (int i=0; i<n-1; i++) {
+            cin>>x>>y;
+            p.insert(make_pair(x, y));
+            if (y > max_y) max_y = y;
+            if (y < min_y) min_y = y;
+        }
+        int b = max_y + min_y;
+        bool is = 1;
+        for (auto it = p.begin(); it!=p.end(); it++) {
+            int x = (*it).first;
+            int y = b - (*it).second;
+            if (p.find(make_pair(x,y))==p.end()) {
+                is = 0;
+                break;
+            }
+        }
+        if (is == 1) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
--- ac-whuoj/ac-5/main.cpp
+++ ac-whuoj/ac-5/main.cpp
@@ -2,31 +2,6 @@
 
 #include <iostream>
 using namespace std;
-/* O(num*value)
-int main(int argc, const char * argv[]) {
-    int dp[101][100001];
-    int s[101],p[101];
-    int num,value;
-    while (cin>>num) {
-        for (int i=1; i<=num; i++) cin>>s[i]>>p[i];
-        cin>>value;
-        for (int i=0; i<=num; i++) dp[i][0]=0;
-        for (int i=0; i<=value; i++) dp[0][i]=0;
-        for (int i=1; i<=num; i++) {
-            for (int j=1; j<=value; j++) {
-                dp[i][j]=dp[i-1][j];
-                if (s[i]<=j) {
-                    if(dp[i][j]<(dp[i-1][j-s[i]]+p[i])) dp[i][j]=(dp[i-1][j-s[i]]+p[i]);
-                }
-            }
-        }
-		cout<<dp[num][value]<<endl;
-    }
-    return 0;
-}
-*/
-
-// O(value)
 int main(){
 	int dp[100001];
 	int s[101],p[101];
